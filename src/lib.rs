54f644dd6ea3ca2e9d313fd62253943a0c93f99c
bigoldcat123 <a1456870251@163.com>
2025-05-25 15:20:04 +0800

    template

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,2 @@
 #![no_std]
 #![allow(static_mut_refs)]
-use embassy_stm32::{
-    adc, bind_interrupts, i2c,
-    peripherals::{self, ADC1},
-    timer::CaptureCompareInterruptHandler,
-};
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
-
-pub mod iic_play;
-pub mod at24c64;
-
-pub static mut CHANNEL: Channel<NoopRawMutex, u32, 3> = Channel::<NoopRawMutex, u32, 3>::new();
-pub static mut CHANNEL2: Channel<NoopRawMutex, u32, 3> = Channel::<NoopRawMutex, u32, 3>::new();
-
-bind_interrupts!(pub struct Irqs {
-    ADC1_2 => adc::InterruptHandler<ADC1>;
-    I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
-    I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
-    TIM2 => CaptureCompareInterruptHandler<embassy_stm32::peripherals::TIM2>;
-    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
-    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
-});
--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,19 @@
 #![allow(static_mut_refs)]
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{
-    adc::Adc,
-    gpio::{Input, Output},
-    peripherals::{self, ADC1, PA0, PA8, PB0, PC13, TIM1},
-    time::Hertz,
-    timer::simple_pwm::{PwmPin, SimplePwm},
-};
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_time::Timer;
-use iic_pi::{CHANNEL, CHANNEL2};
-use {defmt_rtt as _, panic_probe as _};
 
-static mut C: embassy_sync::pubsub::PubSubChannel<NoopRawMutex, i32, 5, 5, 5> =
-    embassy_sync::pubsub::PubSubChannel::new();
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p: embassy_stm32::Peripherals = embassy_stm32::init(Default::default());
-    info!("Hello World!");
-    // let a: peripherals::PB13 = p.PB13;
-    // let led = Output::new(p.PC13, Level::High, Speed::Low);
-    // _spawner.spawn(pc13_receiver(p.PC13)).unwrap();
-    // _spawner.spawn(catch_input(p.PB0)).unwrap();
-    // _spawner.spawn(receiver()).unwrap();
-    // _spawner.spawn(adc(p.ADC1, p.PA0)).unwrap();
-    // _spawner.spawn(ipt(a, led)).unwrap();
+    info!("hello,embed-rust!");
+    let mut led = Output::new(p.PC13, Level::Low, Speed::Medium);
 
-    // _spawner.spawn(pwn(p.PA8, p.TIM1)).unwrap();
-
+    loop {
+        Timer::after_millis(400).await;
+        led.toggle();
+    }
 }
