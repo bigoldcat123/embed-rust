9fa4c3a090e14d119d7393cea19006abc80d44df
bigoldcat123 <a1456870251@163.com>
2025-06-07 23:55:23 +0800

    e

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -4,14 +4,17 @@
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_stm32::gpio::Output;
 use embassy_time::Timer;
 use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let _ = embassy_stm32::init(Default::default());
+    let p = embassy_stm32::init(Default::default());
+    let mut  led = Output::new(p.PC13, embassy_stm32::gpio::Level::Low, embassy_stm32::gpio::Speed::Medium);
     loop {
         info!("hello");
         Timer::after_secs(1).await;
+        led.toggle();
     }
 }
